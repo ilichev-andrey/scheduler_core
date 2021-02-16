e1daeadd4d10d870b0ee3b80e5c6614e555ab460
andreyilichev <ilichev.andrey.y@gmail.com>
2021-02-16 18:04:21 +0300

    Добавлены тесты на сериализацию GetServicesCommand

---
+++ tests/unit_tests/scheduler_core/commands/test_get_services.py
@@ -0,0 +1,48 @@
+import unittest
+
+from ddt import ddt, idata
+
+from scheduler_core.commands.get_services import GetServicesCommand
+from scheduler_core.enums import CommandType
+
+
+def provider_load_from_dict():
+    cases = [
+        # Успешная загрузка данных
+        {
+            'data': {
+                'id': 'command_id'
+            },
+            'expected': {
+                'func_result': True,
+                'id': 'command_id'
+            }
+        }
+    ]
+    for case in cases:
+        yield case
+
+
+@ddt
+class TestGetServicesCommand(unittest.TestCase):
+    @idata(provider_load_from_dict())
+    def test_load_from_dict(self, case_data):
+        data, expected = case_data['data'], case_data['expected']
+
+        response = GetServicesCommand()
+        self.assertEqual(expected['func_result'], response.load_from_dict(data))
+        self.assertEqual(expected['id'], response.id)
+
+    def test_to_dict(self):
+        expected = {
+            'id': 'command_id',
+            'type': CommandType.GET_SERVICES.value
+        }
+
+        response = GetServicesCommand()
+        response.id = 'command_id'
+        self.assertEqual(expected, response.to_dict())
+
+
+if __name__ == '__main__':
+    unittest.main()
